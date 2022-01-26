807b5549f4c51d841211cbe18afe48279d017840
Atharva Deosthale <atharva@Atharvas-MacBook-Pro.local>
2022-01-26 22:59:41 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: "db4free.net",
+    user: "restapitest123",
+    password: "restapitest123",
+    database: "restapitest123",
+  },
+  listPerPage: 10,
+};
+
+module.exports = config;
---
+++ routes/programmingLanguages.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const programmingLanguages = require("../services/programmingLanguages");
+
+/* GET programming languages. */
+router.get("/", async function (req, res, next) {
+  try {
+    res.json(await programmingLanguages.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting programming languages `, err.message);
+    next(err);
+  }
+});
+
+/* POST programming language */
+router.post("/", async function (req, res, next) {
+  try {
+    res.json(await programmingLanguages.create(req.body));
+  } catch (err) {
+    console.error(`Error while creating programming language`, err.message);
+    next(err);
+  }
+});
+
+/* PUT programming language */
+router.put("/:id", async function (req, res, next) {
+  try {
+    res.json(await programmingLanguages.update(req.params.id, req.body));
+  } catch (err) {
+    console.error(`Error while updating programming language`, err.message);
+    next(err);
+  }
+});
+
+/* DELETE programming language */
+router.delete("/:id", async function (req, res, next) {
+  try {
+    res.json(await programmingLanguages.remove(req.params.id));
+  } catch (err) {
+    console.error(`Error while deleting programming language`, err.message);
+    next(err);
+  }
+});
+
+module.exports = router;
